78931bd76f8d656f7385e96fa00b90e569d098b7
Charles <lichazhi@gmail.com>
2018-10-24 07:56:39 -0700

    make it using real time and gravity

--- examples/tutorial/gl/sprays.py
+++ examples/tutorial/gl/sprays.py
@@ -18,15 +18,13 @@ Object (VBO). After each explosion, vertex data for the next explosion are
 calculated, such that each explostion is unique.
 """
 import numpy as np
-
+import time
 from vispy import app
 from vispy.gloo import gl
 
 
 vertex_code = """
 #version 120
-uniform float time;
-uniform vec3 center;
 attribute float lifetime;
 attribute float age;
 attribute vec3 start;
@@ -34,13 +32,13 @@ attribute float visible;
 varying float v_lifetime;
 void main () {
     if (visible > 0 && age > 0 && age < lifetime) {
-        gl_Position.xyz = start + vec3(0, -1.5*age*age, 0);
+        gl_Position.xyz = start + vec3(0, -4.9*age*age, 0);
         gl_Position.w = 1.0;
     } else {
-        // invisible
+        // far far away, invisible
         gl_Position = vec4(-1000, -1000, 0, 0);
         //gl_Position = start;
-        
+
     }
     v_lifetime = clamp((age / lifetime), 0.0, 1.0);
     gl_PointSize = 9 + (v_lifetime * v_lifetime) * 10.0;
@@ -119,7 +117,7 @@ class Canvas(app.Canvas):
         loc = gl.glGetAttribLocation(self.program, "age")
         gl.glEnableVertexAttribArray(loc)
         gl.glVertexAttribPointer(loc, 1, gl.GL_FLOAT, False, stride, offset)
-        
+
         offset += self.data.dtype["age"].itemsize
         loc = gl.glGetAttribLocation(self.program, "start")
         gl.glEnableVertexAttribArray(loc)
@@ -131,7 +129,7 @@ class Canvas(app.Canvas):
         gl.glVertexAttribPointer(loc, 1, gl.GL_FLOAT, False, stride, offset)
 
         # OpenGL initalization
-        self.elapsed_time = 0
+        self.last_time = 0
         gl.glClearColor(0, 0, 0, 1)
         gl.glDisable(gl.GL_DEPTH_TEST)
         gl.glEnable(gl.GL_BLEND)
@@ -150,13 +148,20 @@ class Canvas(app.Canvas):
         gl.glViewport(0, 0, *event.physical_size)
 
     def on_timer(self, event):
+
+        now = time.time()
+        if self.last_time <= 0:
+            self.last_time = now
+            return
+
         n = self.n
-        self.elapsed_time = 1. / 30.
+        elapsed_time = now - self.last_time
+        self.last_time = now
         oldage = np.copy(self.data["age"])
-        self.data["age"] += self.elapsed_time
+        self.data["age"] += elapsed_time
 
         index = np.where(np.logical_or(
-                    np.logical_and(self.data['age']>0, oldage<0), 
+                    np.logical_and(self.data['age']>0, oldage<0),
                     self.data['age']>self.data['lifetime']))   # current_age>0 && (old_age<0) || (current_age>lifetime)
         if self.on:
             np.put(self.data['visible'], index, 1)
@@ -164,11 +169,11 @@ class Canvas(app.Canvas):
             np.put(self.data['visible'], index, 0)
         self.data["age"] %= self.data["lifetime"]
         #print(np.ndarray.sum(self.data["start"]))
-        
+
         gl.glBufferData(gl.GL_ARRAY_BUFFER, self.data, gl.GL_DYNAMIC_DRAW)
-        
-        loc = gl.glGetUniformLocation(self.program, "time")
-        gl.glUniform1f(loc, self.elapsed_time)
+
+        #loc = gl.glGetUniformLocation(self.program, "time")
+        #gl.glUniform1f(loc, self.elapsed_time)
         self.update()
 
     def new_explosion(self):
@@ -179,21 +184,16 @@ class Canvas(app.Canvas):
         loc = gl.glGetUniformLocation(self.program, "color")
         gl.glUniform4f(loc, *color)
 
-        center = np.zeros(3)*.5 # np.random.uniform(-0.5, 0.5, 3)
-        loc = gl.glGetUniformLocation(self.program, "center")
-        gl.glUniform3f(loc, *center)
-
         self.data['lifetime'] = np.random.normal(1.0, 0.005, (n*p,))
-        self.data["age"] = np.repeat(np.arange(-n, 0)*1./90, p) + np.random.normal(0, 0.02, (n*p,)) 
+        self.data["age"] = np.repeat(np.arange(-n, 0)*1./90, p) + np.random.normal(0, 0.2, (n*p,))
         self.data['start'] = np.random.normal(0.0, 0.002, (n*p, 3))
         self.data['start'][:,0] += np.resize(np.linspace(-1.5, 1.5, p), p * n)
         self.data['visible'][:] = 1 #np.random.normal(0.0, 0.002, (n, 3))
-        #self.data['end'][:,1] -= 1 #np.random.normal(0.0, .002, (n, 3))
         gl.glBufferData(gl.GL_ARRAY_BUFFER, self.data, gl.GL_DYNAMIC_DRAW)
 
 if __name__ == '__main__':
     c = Canvas()
-    #c.measure_fps()
+    c.measure_fps()
     c.show()
     #app.set_interactive()
     app.run()
